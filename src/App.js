5c307f9f8d98423b20cd7ee9fb94613a324e394e
makiharu <kitakazetoshirokuma@gmail.com>
2023-10-04 23:30:31 +0900

    focus実装

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import TodoList from './components/TodoList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import * as React from 'react';
 
 function App() {
   // const [todos, setTodos] = useState(["aaaaa","bbb","ccc"]);
   const [todos, setTodos] = useState([{id: 1, name: "Toddo1", completed: false}]);
 
   const todoRef = useRef();
+
+  // focus実装
+  const handleFocus = () => {
+    if(!todoRef.current.value) {
+      todoRef.current.focus();
+    }
+  };
+
+  React.useEffect(() => {
+    handleFocus();
+  }, [todoRef.current]);
+
   const handleAddTodo = () => {
     const name = todoRef.current.value;
 
@@ -19,6 +32,7 @@ function App() {
       return [...prevTodos, {id: uuidv4(), name: name, completed: false}];
     });
     todoRef.current.value = null;
+    handleFocus();
   };
 
   const handleDeleteTodo = () => {
@@ -39,7 +53,7 @@ function App() {
       <div>Hello React</div>
       <TodoList todos={todos} toggleTodo={toggleTodo}/>
       {/* TODO focus実装 */}
-      <input type="text" ref={todoRef}/>
+      <input type="text" ref={todoRef} onChange={handleFocus}/>
       <button onClick={handleAddTodo}>追加</button>
       <button onClick={handleDeleteTodo}>削除</button>
       <div>残りタスク: {todos.filter((todo) => !todo.completed).length}</div>
